4b5c2e2e06f8aef6dc311085ce483b91086700ab
Romeno <romenowenogk@gmail.com>
2019-12-02 12:15:35 +0400

    test

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Text, View, TouchableOpacity } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 
+//test
 export default class CameraExample extends React.Component {
   state = {
     hasCameraPermission: null,
